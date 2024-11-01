f3ae5f209d493f1226c231f47c9766ecedfb1ad8
maziar <sojodyan@gmail.com>
2024-11-01 16:24:13 -0400

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// src/main.rs
+
+// Include the modules for the application
+mod data;
+mod status;
+
 fn main() {
-    println!("Hello, world!");
+    // Entry point of the program
+    println!("Starting the 'entr' application rewrite in Rust...");
+
+    // Example function call from the `status` module
+    status::display_status();
 }
